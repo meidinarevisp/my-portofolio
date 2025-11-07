4d47e4b94bbab0b37044d80d654f50e5cef540d5
meidinarevisp <revimeidina72@gmail.com>
2025-11-07 18:36:22 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -25,7 +25,8 @@ function App() {
   }, [darkMode]);
 
   return (
-    <Router>
+    // Tambahkan basename sesuai nama repo GitHub Pages
+    <Router basename="/my-portofolio">
       <Header darkMode={darkMode} setDarkMode={setDarkMode} />
       <Routes>
         {/* Home Page */}
